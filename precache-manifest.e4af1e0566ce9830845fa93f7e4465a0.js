706808a6376d01658523efb148de7ba1d41e9a05
neypolga1 <63061976+neypolga1@users.noreply.github.com>
2020-05-20 11:55:06 +0000

    Updates

---
+++ precache-manifest.e4af1e0566ce9830845fa93f7e4465a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3183831d532c221c08dbe3c34a2d31c5",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "25cc5dab314d93faffb6",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "18e9244d88dcd6188205",
+    "url": "/robofriends/static/css/main.e207e73b.chunk.css"
+  },
+  {
+    "revision": "25cc5dab314d93faffb6",
+    "url": "/robofriends/static/js/2.18c95c02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.18c95c02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18e9244d88dcd6188205",
+    "url": "/robofriends/static/js/main.0a98aecb.chunk.js"
+  },
+  {
+    "revision": "fff1ce0bcc8205c9b516",
+    "url": "/robofriends/static/js/runtime-main.89784887.js"
+  }
+]);
